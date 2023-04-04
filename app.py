44fdb72122675529f6c7824118f77fce6ee76f1a
xiangyu jia (贾翔宇) <55311659+zsxlh@users.noreply.github.com>
2023-04-04 16:20:31 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, jsonify
+import random
+
+app = Flask(__name__)
+
+choices = {}  # 用于存储用户选择的字典
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    user_name = request.form['user_name']
+    selected_option = int(request.form['selected_option'])
+    choices[user_name] = selected_option
+    return jsonify({"message": "选择已提交。"}), 200
+
+@app.route('/random_assign', methods=['POST'])
+def random_assign():
+    user_name = request.form['user_name']
+    selected_option = random.randint(1, 5)
+    choices[user_name] = selected_option
+    return jsonify({"message": f"用户 {user_name} 被随机分配到选项 {selected_option}"}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
